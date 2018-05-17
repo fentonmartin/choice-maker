1cdf0ceb51f0c5d5da8c016c6b2c735b3e471c65
Fenton Martin <fentonxm@gmail.com>
2018-05-17 13:39:32 +0700

    Update Main Activity

--- app/src/main/java/fen/code/choicemaker/MainActivity.kt
+++ app/src/main/java/fen/code/choicemaker/MainActivity.kt
@@ -1,12 +1,20 @@
 package fen.code.choicemaker
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, PersonActivity::class.java)
+
+        start_button.setOnClickListener{
+            startActivity(intent)
+        }
     }
 }
