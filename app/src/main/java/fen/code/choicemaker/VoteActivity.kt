042103df2f6603ae149ddb85628fa5ba0491496a
Fenton Martin <fentonxm@gmail.com>
2018-05-17 13:40:00 +0700

    Update Vote Activity

--- app/src/main/java/fen/code/choicemaker/VoteActivity.kt
+++ app/src/main/java/fen/code/choicemaker/VoteActivity.kt
@@ -1,12 +1,45 @@
 package fen.code.choicemaker
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_vote.*
 
 class VoteActivity : AppCompatActivity() {
+
+    var countPerson: Int = 0
+    var countTotal: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vote)
+
+        countPerson = intent.getIntExtra("person", 1)
+        countTotal = intent.getIntExtra("total", 1)
+        supportActionBar!!.title = countPerson.toString() + " " + countTotal.toString()
+
+        vote_next.setOnClickListener {
+            val intent: Intent
+            if (countPerson <= 0)
+                intent = Intent(this, ResultActivity::class.java)
+            else
+                intent = Intent(this, VoteActivity::class.java)
+
+            countPerson--
+            intent.putExtra("person", countPerson)
+            intent.putExtra("total", countTotal)
+            startActivity(intent)
+        }
+
+        vote_prev.setOnClickListener {
+            countPerson++
+            if (countPerson >= countTotal) {
+                val intent = Intent(this, VoteActivity::class.java)
+                intent.putExtra("person", countPerson)
+                intent.putExtra("total", countTotal)
+                startActivity(intent)
+            }
+            finish()
+        }
     }
 }
