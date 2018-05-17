10ce7548feb33d216311da6a073fe2006b3cd882
Fenton Martin <fentonxm@gmail.com>
2018-05-17 13:39:45 +0700

    Update Person Activity

--- app/src/main/java/fen/code/choicemaker/PersonActivity.kt
+++ app/src/main/java/fen/code/choicemaker/PersonActivity.kt
@@ -1,12 +1,22 @@
 package fen.code.choicemaker
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_person.*
 
 class PersonActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_person)
+
+        person_button.setOnClickListener {
+            val intent = Intent(this, ChoiceActivity::class.java)
+            intent.putExtra("person", person_text.text.toString().toInt())
+            intent.putExtra("total", person_text.text.toString().toInt())
+
+            startActivity(intent)
+        }
     }
 }
